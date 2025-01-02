70ebc7f3a5ae86840999c6e6bd9a4074aaad7596
Shasawat Pal <shashwatpal2001@gmail.com>
2025-01-02 22:46:35 +0530

    merge conflicts1

--- src/Front.js
+++ src/Front.js
@@ -3,13 +3,15 @@ import './App.css';
 
 function Front(){
     return (
-        <div><h1 className="front"> React 
+        <div>
+            <img className="front_img" height="90" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"/>
+            <h1 className="front"> React 
             <p1 className="front_sub">
-                <p>A JavaScript library for building user interfaces</p>
+                <p>The library for web and native user interfaces</p>
                 </p1>
             <p className="get">
                 <p>Get Started____</p>
-                <p>Take the Tutorial > </p>
+                <p>Take the Tutorial =></p>
             </p>
         </h1></div>
     )
--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -5,19 +5,16 @@ function NavigationBar(){
     return(
         <div className='navbar'>
             <div className='logo'>
-                <img height="20" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"/>
+                <img height="40" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"/>
                 <h3>React</h3>
             </div>
-             
-                <div>Docs</div>
-                <div>Tutorials</div>
-                <div> Blog </div>
-                <div>Community</div>
-
-            <div> Search </div>
-            <div> Language </div>
-            <div> GitHub </div>
-
+                <p className='nav'>Docs</p>
+                <p className='nav'>Tutorials</p>
+                <p className='nav'> Blog </p>
+                <p className='nav'>Community</p>
+                <p className='nav'> Search </p>
+                <p className='nav'> Language </p>
+                <p className='nav'> GitHub </p>
         </div>
     )
 }
